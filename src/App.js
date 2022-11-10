0390d8c13f5e1f23a50260830998513ad024c8a6
FanSusantio <92912460+FanSusantio@users.noreply.github.com>
2022-11-11 02:27:20 +1100

    Customisation of display

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.png";
 import "./App.css";
 import Dictionary from "./Dictionary";
 
@@ -6,11 +5,9 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
-        </header>
+        <header className="App-header">📖 Dictionary word finder</header>
         <main>
-          <Dictionary defaultKeyword="popcorn" />
+          <Dictionary defaultKeyword="tranquil" />
         </main>
         <footer className="App-footer">
           <small>
